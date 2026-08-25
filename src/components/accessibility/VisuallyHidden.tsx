f3006e62a665da9a5de90a12b9e94d7e85b61fe4
praveenprabhakaran-v <praveenprabhakaran.dev@gmail.com>
2026-08-25 13:18:37 +0530

    Empty repositry

--- src/components/accessibility/VisuallyHidden.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const VisuallyHidden: React.FC<{ children: React.ReactNode; as?: React.ElementType }> = ({
-  children,
-  as: Component = 'span',
-}) => {
-  return <Component className="sr-only">{children}</Component>;
-};
